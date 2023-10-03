1f9d2a0ee284a38ae2f637d1f97f373bd2da1435
RNGDebeloper <131392755+RNGDebeloper@users.noreply.github.com>
2023-10-03 19:58:46 +0530

    Update config.py
    Env installed

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("23267035"))
+API_HASH = getenv("a516af48bca80b9f339980650126149c")
+BOT_TOKEN = getenv("6541399786:AAFa17jHfX4IYECp5cTFsR945skTs8bZ_9Y")
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+MONGO_DB_URI = getenv("mongodb+srv://animebot:<password>@cluster0.cybneuo.mongodb.net/?retryWrites=true&w=majority")
 
-INDEX_ID = int(getenv("INDEX_ID"))
-UPLOADS_ID = int(getenv("UPLOADS_ID"))
+INDEX_ID = int(getenv("-1001897930752"))
+UPLOADS_ID = int(getenv("-1001835485195"))
 
-STATUS_ID = int(getenv("STATUS_ID"))
-SCHEDULE_ID = int(getenv("SCHEDULE_ID"))
+STATUS_ID = int(getenv("-1001605018135"))
+SCHEDULE_ID = int(getenv("-1001519241462"))
 
-CHANNEL_TITLE = getenv("CHANNEL_TITLE")
-INDEX_USERNAME = getenv("INDEX_USERNAME")
-UPLOADS_USERNAME = getenv("UPLOADS_USERNAME")
+CHANNEL_TITLE = getenv("Anime Hindi Dubbed")
+INDEX_USERNAME = getenv("anime_hindi_dubbed_by_moviesme")
+UPLOADS_USERNAME = getenv("anime_hubxs")
