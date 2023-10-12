5c41790c6f2f788d8a12f44212733b24bce64988
RNGDebeloper <131392755+RNGDebeloper@users.noreply.github.com>
2023-10-12 20:05:09 +0530

    Update config.py
    updated

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv(" "))
-API_HASH = int(getenv(" "))
-BOT_TOKEN = int(getenv(" "))
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN")
 
-MONGO_DB_URI = int(getenv(" "))
+MONGO_DB_URI = getenv("MONGO_DB_URI")
 
-INDEX_ID = int(getenv(" "))
-UPLOADS_ID = int(getenv(" "))
+INDEX_ID = int(getenv("INDEX_ID"))
+UPLOADS_ID = int(getenv("UPLOADS_ID"))
 
-STATUS_ID = int(getenv(" "))
-SCHEDULE_ID = int(getenv(" "))
+STATUS_ID = int(getenv("STATUS_ID"))
+SCHEDULE_ID = int(getenv("SCHEDULE_ID"))
 
-CHANNEL_TITLE = int(getenv(" "))
-INDEX_USERNAME = int(getenv(" "))
-UPLOADS_USERNAME = int(getenv(" "))
+CHANNEL_TITLE = getenv("CHANNEL_TITLE")
+INDEX_USERNAME = getenv("INDEX_USERNAME")
+UPLOADS_USERNAME = getenv("UPLOADS_USERNAME")
